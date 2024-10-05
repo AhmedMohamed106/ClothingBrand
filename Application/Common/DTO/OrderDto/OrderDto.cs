9d12019daf6d12c617b15e6bfc25b5295e340f19
Ahmed Mohamed <ahmedmohamed204421@gmail.com>
2024-10-05 22:21:56 +0300

    OrderDto

---
+++ Application/Common/DTO/OrderDto/OrderDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothingBrand.Application.Common.DTO.OrderDto
+{
+    public class OrderDto
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string PaymentStatus { get; set; }
+        public string OrderStatus { get; set; }
+        public int ShippingId { get; set; }
+        public int UserId { get; set; }
+        public List<OrderItemDto> OrderItems { get; set; }
+
+    }
+}
