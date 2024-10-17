6fbd3bb71d8273fd341059dac71fcdb81018e75d
Ahmed Mohamed <ahmedmohamed204421@gmail.com>
2024-10-17 11:03:01 +0300

    shippingDto modification

--- Application/Common/DTO/Response/Shipping/ShippingDto.cs
+++ Application/Common/DTO/Response/Shipping/ShippingDto.cs
@@ -11,15 +11,14 @@ namespace ClothingBrand.Application.Common.DTO.Response.Shipping
 {
     public class ShippingDto
     {
-        public int Id { get; set; }
         public string AddressLine1 { get; set; }
-        public string AddressLine2 { get; set; }
+        public string? AddressLine2 { get; set; }
         public string City { get; set; }
         public string State { get; set; }
         public string PostalCode { get; set; }
         public string Country { get; set; }
         public string ShippingMethod { get; set; } // Shipping type like Standard, Express
-        public decimal ShippingCost { get; set; }
+
     }
 
 
